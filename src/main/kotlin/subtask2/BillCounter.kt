5c3415609401932fcf5a72419f7cddd7e18482e7
Andrej Mazur <andrej.mazur@outlook.com>
2021-05-27 23:55:36 +0300

    feat: add second subtask

--- src/main/kotlin/subtask2/BillCounter.kt
+++ src/main/kotlin/subtask2/BillCounter.kt
@@ -2,9 +2,8 @@ package subtask2
 
 class BillCounter {
 
-    // TODO: Complete the following function
-    // The output could be "Bon Appetit" or the string with number(e.g "10")
     fun calculateFairlySplit(bill: IntArray, k: Int, b: Int): String {
-        throw NotImplementedError("Not implemented")
+        val fairlyAmount = (bill.sum() - bill[k]) / 2
+        return if (b <= fairlyAmount) "Bon Appetit" else (b - fairlyAmount).toString()
     }
 }
