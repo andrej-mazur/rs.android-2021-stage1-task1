ca2a74479a458a8e7bc2f3289a0f8f052f58f057
Andrej Mazur <andrej.mazur@outlook.com>
2021-05-27 23:58:29 +0300

    feat: add third subtask

--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -2,8 +2,33 @@ package subtask3
 
 class StringParser {
 
-    // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        val brackets = mapOf(
+            '[' to Pair('[', ']'),
+            '<' to Pair('<', '>'),
+            '(' to Pair('(', ')')
+        )
+        val result = mutableListOf<String>()
+        for (i in inputString.indices) {
+            val iChar = inputString[i]
+            brackets[iChar]?.let { (openingBracket, closingBracket) ->
+                var counter = 0
+                for (j in i + 1 until inputString.length) {
+                    val jChar = inputString[j]
+                    if (jChar == openingBracket) {
+                        counter++
+                    }
+                    if (jChar == closingBracket) {
+                        if (counter == 0) {
+                            val substring = inputString.substring(i + 1, j)
+                            result.add(substring)
+                            break
+                        }
+                        counter--
+                    }
+                }
+            }
+        }
+        return result.toTypedArray()
     }
 }
